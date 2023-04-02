159653d9a35fa9125d83a625e5bccfaaf1134f6d
player01osu <player01osu@gmail.com>
2023-04-02 14:20:19 -0400

    Style: Improve readability and style changes.
    * Use const for variables that are never reassigned.
    * Use let over var (more predictable scoping).
    * Variables renamed to reflect what they are.
    * Break up long lines.

--- scripts/approve.js
+++ scripts/approve.js
@@ -1,14 +1,16 @@
 // Autogrow the size of the textbox to match the space of the poems.
-var auto_grow = (b) => {
-  b.style.height = "5px";
-  b.style.height = b.scrollHeight + "px";
+function auto_grow(element) {
+  element.style.height = "5px";
+  element.style.height = element.scrollHeight + "px";
 }
+
 // Submit an XHTML request to verfiy login information and to obtain database info.
-var login = () => {
-  var b = new XMLHttpRequest();
-  b.open("POST", "./approve.php", !0);
-  b.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  b.onload = () => {
+function login() {
+  const login_request = new XMLHttpRequest();
+
+  login_request.open("POST", "./approve.php", !0);
+  login_request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  login_request.onload = () => {
     if ("wrong" == this.responseText) {
       // If the login is incorrect, inform the user.
       alert("Your login is incorrect");
@@ -16,64 +18,87 @@ var login = () => {
       // If the login is correct, load the database info.
       document.querySelector(".login_container").classList.add("animate__bounceOut");
       document.querySelector(".approve_container").innerHTML = this.responseText;
+
       // For each approve button, create an onclick event.
-      for (var e = document.querySelectorAll(".approve_btn"), c = 0; c < e.length; c++) {
-        e[c].onclick = () => {
-          var a = "true", d = "";
+      const approve_buttons = document.querySelectorAll(".approve_btn");
+      for (let i = 0; i < approve_buttons.length; ++i) {
+        approve_buttons[i].onclick = () => {
+          let edited = true, org = "";
           // When the approve button is selected, check to see if the poem has been edited.
-          document.getElementById(this.id).value == document.getElementById(this.id + "org").value ? a = "false" : d = document.getElementById(this.id + "org").value;
-          var f = new XMLHttpRequest();
-          f.open("POST", "./approve.php", !0);
-          f.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-          f.onload = () => {
+          if (document.getElementById(this.id).value == document.getElementById(this.id + "org").value) edited = false;
+          else org = document.getElementById(this.id + "org").value;
+
+          const approve_request = new XMLHttpRequest();
+          approve_request.open("POST", "./approve.php", !0);
+          approve_request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+          approve_request.onload = () => {
             // If there is an issue with approving a poem, notify the user.
             "success" != this.responseText && alert("Error: could not approve poem.");
           };
+
           // Submit an xHTML request to update the database to approve the poem.
-          f.send("username=" + encodeURI(document.getElementById("name").value) + "&password=" + encodeURI(document.getElementById("password").value) + "&approve=" + this.id + "&content=" + encodeURI(document.getElementById(this.id).value) + "&edited=" + a + "&org=" + d);
+          approve_request.send(
+            "username=" + encodeURI(document.getElementById("name").value) +
+              "&password=" + encodeURI(document.getElementById("password").value) +
+              "&approve=" + this.id +
+              "&content=" + encodeURI(document.getElementById(this.id).value) +
+              "&edited=" + edited +
+              "&org=" + org
+          );
           // Hide the poem from the table listing.
           this.parentElement.parentElement.remove();
         };
       }
+
       // Create an array of all discard buttons.
-      e = document.querySelectorAll(".delete_btn");
-      for (c = 0; c < e.length; c++) {
+      const delete_btn = document.querySelectorAll(".delete_btn");
+      for (let i = 0; i < delete_btn.length; ++i) {
         // Create an onclick event for each discard button in the table.
-        e[c].onclick = () => {
-          var a = new XMLHttpRequest();
-          a.open("POST", "./approve.php", !0);
-          a.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-          a.onload = () => {
+        delete_btn[i].onclick = () => {
+          const approve_request = new XMLHttpRequest();
+          approve_request.open("POST", "./approve.php", !0);
+          approve_request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+          approve_request.onload = () => {
             // If there is an issue with discarding a poem, notify the user.
             "success" != this.responseText && alert("error");
           };
-          a.send("username=" + encodeURI(document.getElementById("name").value) + "&password=" + encodeURI(document.getElementById("password").value) + "&remove=" + this.id);
+          approve_request.send(
+            "username=" + encodeURI(document.getElementById("name").value) +
+              "&password=" + encodeURI(document.getElementById("password").value) +
+              "&remove=" + this.id
+          );
           // Hide the poem from the table listing.
           this.parentElement.parentElement.remove();
         };
       }
+
       setTimeout(() => {
         document.querySelector(".login_container").style.display = "none";
         document.querySelector(".approve_container").style.display = "block";
         document.querySelector(".approve_container").classList.add("animate__bounceIn");
         // Adjust the size of each textbox to the space the text takes up.
-        for (var a = document.querySelectorAll("textarea"), d = 0; d < a.length; d++) {
-          auto_grow(a[d]);
+        const text_areas = document.querySelectorAll("textarea");
+        for (let i = 0; i < text_areas.length; ++i) {
+          auto_grow(text_areas[i]);
         }
       }, 700);
     }
   };
+
   // Send an xHTML request to verfiy the login info to the site.
-  b.send("username=" + encodeURI(document.getElementById("name").value) + "&password=" + encodeURI(document.getElementById("password").value));
+  login_request.send(
+    "username=" + encodeURI(document.getElementById("name").value) +
+      "&password=" + encodeURI(document.getElementById("password").value)
+  );
 }
+
 // When the login button is clicked, login to the website.
-document.querySelector(".login").onclick = () => {
-  login();
-};
+document.querySelector(".login").onclick = () => login();
+
 // When enter is tapped on the keyboard, login to the website.
-var inputs = document.querySelectorAll("input");
-for(var i = 0; i < inputs.length; i++) {
+const inputs = document.querySelectorAll("input");
+for (let i = 0; i < inputs.length; i++) {
   inputs[i].onkeydown = (e) => {
-    if(e.which == 13) login();
+    if(e.which === 13) login();
   }
 }
--- scripts/main_script.js
+++ scripts/main_script.js
@@ -1,32 +1,34 @@
 // Define the max poem character length
 const MAX_POEM_LENGTH = 5000;
+
 // After the name and email form is submitted, hide the get_started_container and display the write_containr.
 document.querySelector("form").onsubmit = (e) => {
+  e.preventDefault();
   document.querySelector(".get_started_container").classList.add("animate__bounceOut");
+
   setTimeout(() => {
     document.querySelector(".get_started_container").style.display = "none";
     document.querySelector(".write_container").classList.add("animate__bounceIn");
     document.querySelector(".write_container").style.display = "block";
   }, 650);
-  setTimeout(() => {
-    document.querySelector(".write_container").classList.remove("animate__bounceIn");
-  }, 1500);
-  e.preventDefault();
+
+  setTimeout(() => document.querySelector(".write_container").classList.remove("animate__bounceIn"), 1500);
+
   return false;
 };
+
 // When the poem is submitted, hide the write container window and display a confirmation.
 document.querySelector(".submit_poem").onclick = () => {
   document.querySelector(".write_container").classList.add("animate__backOutRight");
-  setTimeout(() => {
-    document.querySelector(".write_container").style.display = "none";
-  }, 1000);
-  var xhr = new XMLHttpRequest();
+  setTimeout(() => document.querySelector(".write_container").style.display = "none", 1000);
+
+  const xhr = new XMLHttpRequest();
   xhr.open("POST", "./", true);
   xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   xhr.onload = () => {
     if(this.responseText != "error") {
       // If the poem is submitted sucessfully, display a success window.
-      var first_name = document.getElementById("name").value.replace(/ .*/, "");
+      const first_name = document.getElementById("name").value.replace(/ .*/, "");
       document.querySelector(".poem_id").innerHTML = this.responseText;
       document.querySelector(".submit_name").innerHTML = first_name;
       document.querySelector(".success_container").style.display = "block";
@@ -39,8 +41,13 @@ document.querySelector(".submit_poem").onclick = () => {
     }
   };
   // Submit an xHTML request with the name, email,and poem content to the server.
-  xhr.send("name=" + encodeURI(document.getElementById("name").value) + "&email=" + encodeURI(document.getElementById("email").value) + "&content=" + encodeURI(document.querySelector(".textarea_poem").value));
+  xhr.send(
+    "name=" + encodeURI(document.getElementById("name").value) +
+      "&email=" + encodeURI(document.getElementById("email").value) +
+      "&content=" + encodeURI(document.querySelector(".textarea_poem").value)
+  );
 };
+
 // As text is entered into the poem textbox, update the characters remaining indicator.
 document.querySelector("textarea").oninput = () => {
   document.querySelector(".text_limit").innerHTML = MAX_POEM_LENGTH - this.value.length + " characters remaining";
