03980fd47a50d771d1571fa78661650bb84df844
Caeden Kidd <caedenmkidd@gmail.com>
2024-07-23 03:04:48 -0400

    Revert "Add files via upload"

--- picture_generator.php
+++
@@ -1,5 +0,0 @@
-<?php 
-    # Include the Picture Generator you prefer according to your needs.
-    # As of July 21st, please use "./picture_generators/gd.php"
-    include "./picture_generators/gd.php";
-?>
